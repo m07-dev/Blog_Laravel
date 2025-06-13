c02505e526374a1df2a95c5e7784265c140c70b1
Mohamed <fofanaa.mohamed45@gmail.com>
2025-06-13 18:17:26 +0200

    ajout des nouvelle methode delete, createPost, one post

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -15,11 +15,12 @@ class MyController extends Controller
 {
     public function createPost()
     {
-        $post = new Post();
+        /*$post = new Post();
         $post->title = 'Life';
         $post->content = 'Life is what happens while you are busy making other plans';
         $post->save();
-        return $post;
+        return $post;*/
+        return view('createPost');
     }
 
     public function affichePost(Request $request)
@@ -31,16 +32,33 @@ class MyController extends Controller
         $post->title = $title;
         $post->content = $content;
         $post->save();
-        
+
         $listePosts = Post::all();
         return view('acceuilBlog', ['list' => $listePosts]);
     }
 
-    // Création de données pour les étudiant et la formation
-    public function create(){
 
-        $L2Info = Formations::find(1) ;
+    public function one_post($id)
+    {
+        $post = Post::find($id);
+        return view('viewpost', ['post' => $post]);
+    }
+
+    // Création de données pour les étudiant et la formation
+    public function create()
+    {
+
+        $L2Info = Formations::find(1);
         echo $L2Info->intitule;
-        
+    }
+    // Supprimer un post :
+    public function deletePost( $id)
+    {
+        $post = Post::find($id);
+        if ($post) {
+            $post->delete();
+        }
+        $listePosts = Post::all();
+        return redirect('/acceuilBlog');
     }
 }
